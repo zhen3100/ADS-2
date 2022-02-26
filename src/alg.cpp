86c2fe7118a10c9c1574a8b91c227c24ec32392e
Evgeny Volkov <88347990+zhen3100@users.noreply.github.com>
2022-02-26 18:29:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,48 @@
 
 
 double pown(double value, uint16_t n) {
-
+    double res = 1;
+    for (uint16_t i = 1; i <= n; i++) {
+        res = res * value;
+    }
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
-
+    uint64_t res = 1;
+    uint16_t i = 1;
+    while (n >= i) {
+        res = res * i;
+        i++;
+    }
+    return res;
 }
 
 double calcItem(double x, uint16_t n) {
-
+    double res = (pown(x, n) / fact(n));
+    return res;
 }
 
 double expn(double x, uint16_t count) {
-
+    double res = 0;
+    for (uint16_t n = 0; n < count; n++) {
+        res += pown(x, n) / fact(n);
+    }
+    return res;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double res = 0;
+    for (uint16_t n = 1; n <= count; n++) {
+        res += pown(-1, (n - 1)) * calcItem(x, (2 * n - 1));
+    }
+    return res;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double res = 0;
+    for (uint16_t n = 1; n <= count; n++) {
+        res += pown(-1, (n - 1)) * calcItem(x, (2 * n - 2));
+    }
+    return res;
 }
